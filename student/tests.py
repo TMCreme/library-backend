016796c3c12e1aba62df13b7b9ea65bc5024efd7
Charles Agoe <agoetee@gmail.com>
2024-10-29 16:02:49 +0000

    test: test for student credentials

--- student/tests.py
+++ student/tests.py
@@ -4,4 +4,15 @@ from student.models import Student
 
 
 class StudentTests(TestCase):
-    pass
+    @classmethod
+    def setUpTestData(cls):
+        cls.student = Student.objects.create(
+            student_id="1231231",
+            first_name="Kofi",
+            last_name="Ntiamoah",
+        )
+
+    def test_student_attributes(self):
+        self.assertEqual(self.student.student_id, "1231231")
+        self.assertEqual(self.student.first_name, "Kofi")
+        self.assertEqual(self.student.last_name, "Ntiamoah")
